2642db1d04817016e5d1fae77c064a4971f2d155
Vincent Reilly <reilly.vincent@gmail.com>
2024-03-21 17:54:57 +1300

    Improving detail modal content. Still does not conform to design though

--- src/DetailModal.tsx
+++ src/DetailModal.tsx
@@ -42,17 +42,16 @@ export default function DetailView({ closeCallback, isVisible, food }: { closeCa
           <Card style= {{backgroundColor: COLOURS.primary}}>
             {food ? (
                 <React.Fragment>
-                    <Card.Title title={food.name} subtitle="Card Subtitle" left={LeftContent} />
-                    <Card.Content>
-                    <PaperText variant="titleLarge">{food.name}</PaperText>
-                    <PaperText variant="bodyMedium">Card content</PaperText>
-                    </Card.Content>
+                    <Card.Title titleVariant="titleLarge" title={food.name} subtitle="Card Subtitle" />
+                    {/* <Card.Content> */}
+                    {/* <PaperText variant="bodyMedium">Card content</PaperText> */}
+                    {/* </Card.Content> */}
+                    <Card.Cover style= {{padding: 16, backgroundColor: COLOURS.primary}} source={food.image} />
                 </React.Fragment>
             ) : null}
-            <Card.Cover style= {{padding: 16, backgroundColor: COLOURS.primary}} source={{ uri: 'https://picsum.photos/700' }} />
             <Card.Actions>
-              <Button onPress={closeCallback}>Cancel</Button>
-              <Button>Ok</Button>
+              {/* <Button onPress={closeCallback}>Cancel</Button> */}
+              <Button onPress={closeCallback}>Ok</Button>
             </Card.Actions>
           </Card>
         </View>
